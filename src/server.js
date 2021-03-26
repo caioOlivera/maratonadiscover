51f9a6b084385f3b7c49e9f678a6f31da399d797
Caio Oliveira <caio.goncalves.pro@gmail.com>
2021-03-25 23:17:27 -0300

    commit aula 1

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express();
+const routes = require("./routes");
+
+//usando template engine (js no html)
+server.set("view engine", "ejs");
+
+//habilitar  arquivos statics
+server.use(express.static("public"));
+
+//routes
+server.use(routes);
+server.listen(3000, () => console.log("rodando"));
